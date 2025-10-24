4b18bbfcf5aac61fdbce4138e6feca102b681980
Rémi Alban <remi.alban.1@ens.etsmtl.ca>
2025-10-24 13:29:29 -0400

    add tests

---
+++ src/test/java/ca/log430/transactions/TransactionTest.java
@@ -0,0 +1,13 @@
+package ca.log430.transactions;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TransactionTest {
+
+    // add tests here if needed
+    public void sampleTest() {
+        assert(true);
+    }
+}
