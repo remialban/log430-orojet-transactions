41d7088e01b3305001478a79a3f83145817342b1
Rémi Alban <remi.alban.1@ens.etsmtl.ca>
2025-10-24 13:27:22 -0400

    add tests

--- src/test/java/ca/log430/transactions/TransactionsApplicationTests.java
+++ src/test/java/ca/log430/transactions/TransactionsApplicationTests.java
@@ -3,11 +3,15 @@ package ca.log430.transactions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TransactionsApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+        assertEquals(true, true);
 	}
 
 }
