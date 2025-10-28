b48c1c7631b97e41490c475bdf0665f74df991f3
Rémi Alban <remi.alban.1@ens.etsmtl.ca>
2025-10-28 17:34:44 -0400

    refactor: update TransactionTest to include order repository interactions and assertions

--- src/test/java/ca/log430/transactions/TransactionTest.java
+++ src/test/java/ca/log430/transactions/TransactionTest.java
@@ -1,11 +1,18 @@
 package ca.log430.transactions;
 
 
+import ca.log430.transactions.domain.model.Ordre;
+import ca.log430.transactions.domain.model.OrdreType;
+import ca.log430.transactions.ports.out.OrderRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TransactionTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     // add tests here if needed
     public void sampleTest() {
@@ -14,9 +21,25 @@ public class TransactionTest {
 
     @Test
     void createTransaction() {
-        Transaction transaction = new Transaction("tx123", 100.0, "user456");
-        assert(transaction.getId().equals("tx123"));
-        assert(transaction.getAmount() == 100.0);
-        assert(transaction.getUserId().equals("user456"));
+        Ordre ordre = new Ordre();
+        ordre.setType(OrdreType.ACHAT);
+        ordre.setAmount(100);
+
+        this.orderRepository.save(ordre);
+
+        Ordre ordre2 = this.orderRepository.findById(ordre.getId()).orElse(null);
+
+        assert(ordre2 != null);
+        assert(ordre2.getType() == OrdreType.ACHAT);
+        assert(ordre2.getAmount() == 100);
+
+        this.orderRepository.deleteById(ordre2.getId());
+
+        if (this.orderRepository.findById(ordre2.getId()).isPresent()) {
+            assert(false);
+        } else {
+            assert(true);
+        }
+
     }
 }
